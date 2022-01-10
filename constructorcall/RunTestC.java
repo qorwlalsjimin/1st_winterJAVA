8dbcf61803736383ff9b5e1e0b0c96bb68ce121d
Baek Jimin <s2104@e-mirim.hs.kr>
2022-01-10 11:51:04 +0900

    Add files via upload

---
+++ constructorcall/RunTestC.java
@@ -0,0 +1,10 @@
+package test.constructorcall;
+
+public class RunTestC {
+
+	public static void main(String[] args) {
+		new TestC();
+
+	}
+
+}
---
+++ constructorcall/TestC.java
@@ -0,0 +1,8 @@
+package test.constructorcall;
+
+public class TestC extends TestB {
+
+	TestC(){
+		System.out.println("TestC() 생성자 호출");
+	}
+}
