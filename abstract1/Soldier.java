fc3dab5d3c9215beb0bc4a9c61a5558fda554342
Baek Jimin <s2104@e-mirim.hs.kr>
2022-01-11 11:52:32 +0900

    Add files via upload

---
+++ abstract1/Soldier.java
@@ -0,0 +1,14 @@
+package test.abstract1;
+
+public class Soldier extends Weapon3 {
+
+	@Override
+	public void fire() {
+		System.out.println("ÅÁÅÁÅÁ");
+	}
+	
+	public void walk() {
+		System.out.println("±ºÀÎÀÌ °È´Â´Ù.");
+	}
+
+}
